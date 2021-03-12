99b5e9dce62ea803f32ab679adb53cc385e9ae30
todoesverso <todoesverso@gmail.com>
2021-03-11 21:18:46 -0300

    Move back to the other pc

--- src/actors/Enemy.gd
+++ src/actors/Enemy.gd
@@ -1,45 +1,45 @@
 extends Actor
 
+var pidgeons: Array = ['p1', 'p2', 'p3']
+onready var anim_player:  AnimationPlayer = get_node("Sprite/AnimationPlayer")
 var hit_manu: bool = false
 
 func _ready() -> void:
-    gravity = 10.0    
-    set_physics_process(false)
-    _velocity.x = -speed.x
+	speed = Vector2(40.0, 100.0)
+	gravity = 10.0    
+	set_physics_process(false)
+	_velocity.x = -speed.x
+	anim_player.play(pidgeons[randi() % pidgeons.size()])
 
 func _on_HitPlayer_body_entered(body: Node) -> void:
-    if body.name == "Manu":
-        _velocity = calculate_stomp_velocity(_velocity, 30)
-        hit_manu = true
+	if body.name == "Manu":
+		_velocity = calculate_stomp_velocity(_velocity, 30)
+		hit_manu = true
 
 func _on_HitPlayer_body_exited(body: Node) -> void:
-    if body.name == "Manu":
-        hit_manu = false
+	if body.name == "Manu":
+		hit_manu = false
 
 func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
-    if body and body.global_position.y > get_node("StompDetector").global_position.y:
-        return
-    get_node("CollisionShape2D").disabled = true	
-    queue_free() 
+	if body and body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	die()
 
-func _on_VisibilityEnabler2D_screen_exited() -> void:
-    get_node("CollisionShape2D").disabled = true	
-    queue_free()
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+	die()
 
 func _physics_process(delta: float) -> void:
-    _velocity.y += gravity * delta
-    if is_on_wall() and not hit_manu:
-        _velocity.x *= -1.0
-    _velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+	_velocity.y += gravity * delta
+	if is_on_wall() and not hit_manu:
+		_velocity.x *= -1.0
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
 
 func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2: 
-    var out: = linear_velocity
-    out.y =  -impulse
-    out.x = -impulse
-    return out
+	var out: = linear_velocity
+	out.y =  -impulse
+	out.x = -impulse
+	return out
 
-
-
-
-
-
+func die() -> void:
+	get_node("CollisionShape2D").disabled = true	
+	queue_free()
--- src/actors/Manu.gd
+++ src/actors/Manu.gd
@@ -12,89 +12,89 @@ var facing_right: bool = true
 var hurt: bool = false
 
 func _on_EnemyDetector_area_entered(area: Area2D) -> void:
-    _velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
-    if anim_name == "hurt":
-        hurt = false
+	if anim_name == "hurt":
+		hurt = false
 
 
 func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
-    hurt = true
-    hitpoints -= hit_costs
-    health_bar.set_percent_value_int(float(hitpoints)/max_hitpoints * 100)
-    if hitpoints <= 0:
-        die()
+	hurt = true
+	hitpoints -= hit_costs
+	health_bar.set_percent_value_int(float(hitpoints)/max_hitpoints * 100)
+	if hitpoints <= 0:
+		die()
 
 func die() -> void:
-    pass
+	pass
 
 func _physics_process(delta: float) -> void:
-    var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0 
-    var direction: = get_direction()
-    _velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-    _velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-    animate(_velocity)
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0 
+	var direction: = get_direction()
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	animate(_velocity)
 
 func get_direction() -> Vector2:
-    return Vector2(
-        Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-        -1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
-        )
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
+		)
 
 func calculate_move_velocity(
-        linear_velocity: Vector2,
-        direction: Vector2,
-        speed: Vector2,
-        is_jump_interrupted: bool
-    ) -> Vector2:
-        
-    var out: = linear_velocity
-    out.x = speed.x * direction.x
-    out.y += gravity * get_physics_process_delta_time()
-    if direction.y == -1.0:
-        out.y = speed.y * direction.y
-    if is_jump_interrupted:
-        out.y = 0.0
-    return out
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+		
+	var out: = linear_velocity
+	out.x = speed.x * direction.x
+	out.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		out.y = speed.y * direction.y
+	if is_jump_interrupted:
+		out.y = 0.0
+	return out
 
 func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2: 
-    var out: = linear_velocity
-    out.y =  -impulse
-    return out
+	var out: = linear_velocity
+	out.y =  -impulse
+	return out
 
 func animate(direction: Vector2) -> void:
-    if hurt:
-        anim_player.play("hurt")
-        return
-    
-    if direction.x < 0:
-        anim_player.play("walk_left")
-        facing_right = false
-    
-    if direction.x > 0:
-        anim_player.play("walk_right")
-        facing_right = true
-        
-    if facing_right:
-        if direction.y < 0:
-            anim_player.play("jump_down_right")
+	if hurt:
+		anim_player.play("hurt")
+		return
+	
+	if direction.x < 0:
+		anim_player.play("walk_left")
+		facing_right = false
+	
+	if direction.x > 0:
+		anim_player.play("walk_right")
+		facing_right = true
+		
+	if facing_right:
+		if direction.y < 0:
+			anim_player.play("jump_down_right")
 
-        if direction.y > 0:
-            anim_player.play("jump_up_right")
-    else:
-        if direction.y < 0:
-            anim_player.play("jump_down_left")
+		if direction.y > 0:
+			anim_player.play("jump_up_right")
+	else:
+		if direction.y < 0:
+			anim_player.play("jump_down_left")
 
-        if direction.y > 0:
-            anim_player.play("jump_up_left") 
+		if direction.y > 0:
+			anim_player.play("jump_up_left") 
 
-    
-    if direction.x == 0 and direction.y == 0:
-        if facing_right:
-            anim_player.play("idle_right")
-        else:
-            anim_player.play("idle_left")
+	
+	if direction.x == 0 and direction.y == 0:
+		if facing_right:
+			anim_player.play("idle_right")
+		else:
+			anim_player.play("idle_left")
 
 
 
